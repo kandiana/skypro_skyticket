e7f1d43f21f5300619b77cded079787efe7edab6
Artem <a.marinskih1@gmail.com>
2021-10-28 10:12:01 +0500

    added reactcomponent as logo in header

--- projects/selling-tickets/src/components/Header/Header.tsx
+++ projects/selling-tickets/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import './Header.scss'
-import logo from '../../assets/images/logo.svg'
+import { ReactComponent as Logo } from '../../assets/images/logo.svg'
 
 export type props = {
     headerTitle: string
@@ -11,7 +11,7 @@ export const Header: FC<props> = ({headerTitle}) => {
  
     return (
         <header className='Header' >
-            <img className='Header__logo' src={logo} alt="логотип" />
+            <Logo className='Header__logo' />
             <h1 className='Header__title'>{headerTitle}</h1>
         </header>       
     );
