cc3ac18024c60b32d890d3c2243915edcd570708
Neo1338 <latevk@gmail.com>
2021-10-28 22:51:51 +0700

    Add Input

--- projects/selling-tickets/src/App.tsx
+++ projects/selling-tickets/src/App.tsx
@@ -1,16 +1,16 @@
 import React, { FC } from 'react';
 import './App.scss';
 import { Button } from './components/Button/Button';
-import { inputSearch } from './components/inputSearch/inputSearch';
+import { InputText } from './components/InputText/InputText';
 
 const App: FC = () => {
   const handleClick = () => {
-    console.log('click')
-  }
+    console.log('click');
+  };
 
   return (
     <div className="App">
-      <inputSearch />
+      <InputText value=''/>
       <Button buttonText='Купить' handleClick={handleClick}/>
     </div>
   );
