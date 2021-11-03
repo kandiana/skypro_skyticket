b3c24741ed388f282f8257d3ebb81a1346f7d001
kandiana <dianka95@gmail.com>
2021-11-03 10:43:32 +0300

    added example on how to control components style with classes

--- projects/selling-tickets/src/components/Button/Button.tsx
+++ projects/selling-tickets/src/components/Button/Button.tsx
@@ -1,15 +1,32 @@
 import { FC } from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
 export type props = {
-  buttonText: string;
   handleClick: () => void;
+  size?: 'S' | 'M' | 'L';
+  color?: 'red' | 'blue';
 };
 
-export const Button: FC<props> = ({ buttonText, handleClick }) => {
+export const Button: FC<props> = ({
+  children,
+  handleClick,
+  size = undefined,
+  color = undefined,
+}) => {
+  const buttonClasses = classNames(
+    'Button',
+    {
+      Button_small: size === 'S',
+      Button_medium: size === 'M',
+      Button_large: size === 'L',
+    },
+    { Button_red: color === 'red', Button_blue: color === 'blue' }
+  );
+
   return (
-    <button className="Button" onClick={handleClick}>
-      {buttonText}
+    <button className={buttonClasses} onClick={handleClick}>
+      {children}
     </button>
   );
 };
