4cdc4c46383982f04b4ebaa5f8328351b9d7a338
kandiana <dianka95@gmail.com>
2021-11-04 13:02:02 +0300

    added multer, configured first version of image uploading

--- projects/backend/src/controllers/test/insertItem.js
+++ projects/backend/src/controllers/test/insertItem.js
@@ -1,6 +1,8 @@
 module.exports = (req, res) => {
   const db = req.db;
 
+  // console.log(req);
+
   db.collection('test').insertOne(req.body, (err, result) => {
     if (err) {
       res.send({ error: 'An error has occurred' });
--- projects/backend/src/routers.js
+++ projects/backend/src/routers.js
@@ -1,5 +1,21 @@
 const { Router } = require('express');
 
+const { imagesFolder } = require('./config');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, imagesFolder);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const extension = file.mimetype.split('/')[1];
+    req.body.img = `${uniqueSuffix}.${extension}`;
+    cb(null, req.body.img);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 const ping = require('./controllers/ping');
 const test = require('./controllers/test');
 // const events = require('./controllers/events');
@@ -9,7 +25,7 @@ const testRouter = new Router();
 
 testRouter.get('/one/:id', test.readItem);
 testRouter.get('/all', test.readAllItems);
-testRouter.post('/new', test.insertItem);
+testRouter.post('/new', upload.single('image'), test.insertItem);
 
 const eventsRouter = new Router();
 
