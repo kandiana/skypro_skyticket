9574a384df85298e00f47138acbb972f5df3bbc7
Artem_Marinskih <a.marinskih1@gmail.com>
2021-11-05 17:53:02 +0500

    added case array/get

--- projects/selling-tickets/src/store/store.ts
+++ projects/selling-tickets/src/store/store.ts
@@ -1,6 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { reducer } from './reducers';
+import axios from 'axios';
+import { arrCards } from '../components/CardsContainer/CardsContainer';
 
 export const store = createStore(reducer, applyMiddleware(thunk));
 
