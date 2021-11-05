ffeb334e5fdb47871508b6d7047790b5609dff58
Neo1338 <latevk@gmail.com>
2021-11-05 18:14:35 +0700

    Edit conflicts 2

--- projects/selling-tickets/src/components/Button/Button.tsx
+++ projects/selling-tickets/src/components/Button/Button.tsx
@@ -1,14 +1,29 @@
 import { FC } from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
-export type Props = {
-  buttonText: string;
+export type props = {
   handleClick: () => void;
   size?: 'S' | 'M' | 'L';
   color?: 'red' | 'blue';
 };
 
-export const Button: FC<Props> = ({ buttonText, handleClick }) => {
+export const Button: FC<props> = ({
+  children,
+  handleClick,
+  size = undefined,
+  color = undefined,
+}) => {
+  const buttonClasses = classNames(
+    'Button',
+    {
+      Button_small: size === 'S',
+      Button_medium: size === 'M',
+      Button_large: size === 'L',
+    },
+    { Button_red: color === 'red', Button_blue: color === 'blue' }
+  );
+
   return (
     <button className={buttonClasses} onClick={handleClick}>
       {children}
