c4b1772c1ce483d257f34a380a21e998bf742b84
Artem_Marinskih <a.marinskih1@gmail.com>
2021-11-05 14:48:53 +0500

    add newArrayCards in store

--- projects/selling-tickets/src/index.tsx
+++ projects/selling-tickets/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.scss';
 import App from './App';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './store/store';
+
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
--- projects/selling-tickets/src/store/store.ts
+++ projects/selling-tickets/src/store/store.ts
@@ -1,4 +1,6 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { arrCards } from '../components/CardsContainer/CardsContainer';
 
 const INITIAL_STATE = {
   formData: {
@@ -11,17 +13,30 @@ const INITIAL_STATE = {
 
 // @ts-ignore
 function reducer(state = INITIAL_STATE, action) {
+  console.log(action)
   switch (action.type) {
     case 'form/filter':
       return {
         ...state,
         formData: action.form,
       };
+    case 'arr/cards':
+      const newArrayCards = [];
+
+      for(let i = 0; i < arrCards.length; i++) {
+        newArrayCards.push(arrCards[i])
+      }
+
+      return {
+        ...state,
+        console: console.log('Пришли из CardsConteiner'),
+        cardsData: [...newArrayCards]
+      };
     default:
       return state;
   }
 }
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, applyMiddleware(thunk));
 
 store.subscribe(() => console.log(store.getState()));
