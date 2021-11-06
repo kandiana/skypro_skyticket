3098da1f8d97506e897181fd256288e7d34b2d4c
kandiana <dianka95@gmail.com>
2021-11-06 19:02:08 +0300

    fixed types errors

--- projects/selling-tickets/src/store/actions.ts
+++ projects/selling-tickets/src/store/actions.ts
@@ -1,14 +1,16 @@
+import { EventDataShort } from '../components/CardsContainer/CardsContainer';
 import { FormType } from '../pages/MainPage/MainPage';
 
 export const FORM_FILTER_ACTION = 'FORM@FILTER' as const;
 export const ARR_CARDS_ACTION = 'ARR@CARDS' as const;
+export const TEST_BACK_ACTION = 'TEST_BACK' as const;
 
 export function getFormFilter(form: FormType) {
   return { type: FORM_FILTER_ACTION, form };
 }
 
-export function getArrCards() {
-  return { type: ARR_CARDS_ACTION };
+export function getArrCards(eventCards: EventDataShort[]) {
+  return { type: ARR_CARDS_ACTION, eventCards };
 }
 
 export type FormFilterActionType = ReturnType<typeof getFormFilter>;
