5ebbc5093e60b92e26a0e10630bebc5e5ffa8ff8
kandiana <dianka95@gmail.com>
2021-11-09 14:00:27 +0300

    moved ObjectId creation into try-catch and added russian error essages to checkTicket handler

--- projects/backend/src/controllers/events/readOneEvent.js
+++ projects/backend/src/controllers/events/readOneEvent.js
@@ -4,9 +4,9 @@ module.exports = async (req, res) => {
   const db = req.db;
   const { id } = req.params;
 
-  const filter = { _id: new ObjectId(id) };
-
   try {
+    const filter = { _id: new ObjectId(id) };
+
     const event = await db.events.findOne(filter);
 
     if (!event) {
