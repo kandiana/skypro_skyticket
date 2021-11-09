f24f331de9e7d7e33f2d55290924225fbc8ee861
Artem <a.marinskih1@gmail.com>
2021-11-09 09:43:19 +0500

    fix rebase

--- projects/selling-tickets/src/store/store.ts
+++ projects/selling-tickets/src/store/store.ts
@@ -1,7 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import axios from 'axios';
-import { arrCards } from '../components/CardsContainer/CardsContainer';
 import { reducer } from './reducers';
 
 export const store = createStore(reducer, applyMiddleware(thunk));
