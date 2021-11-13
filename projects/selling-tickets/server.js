20f74f4102a18fdf1937741a25edf508d4a6b1b7
Artem_Marinskih <a.marinskih1@gmail.com>
2021-11-13 17:39:36 +0500

    fix pacage.json and added server.js

---
+++ projects/selling-tickets/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
